8bedc4ee87cbbe53d3cc06c3bbbbf309a45f9ea8
Name-Nils <84877176+Name-Nils@users.noreply.github.com>
2022-06-04 16:19:16 +0200

    forgot one file

--- code/Python/lidar.py
+++ code/Python/lidar.py
@@ -14,11 +14,13 @@ def get_data():
 
 
 class Point:
-    def __init__(self, angle, dist, acc = 1):
+    def __init__(self, angle, dist, robot_x = 0, robot_y = 0, acc = 1):
         self.angle = angle
         self.dist = dist
         self.acc = acc
 
+        self.robot_x = robot_x
+        self.robot_y = robot_y
         self.x = None
         self.y = None
 
@@ -34,7 +36,15 @@ class Set:
             point.x = math.cos(point.angle) * point.dist
             point.y = math.sin(point.angle) * point.dist
 
-    def calc_acc(self):
+    def translate(self, x, y, r): # rotates first then adds the offset
+        for point in self.points:
+            new_x = (point.x * math.cos(r)) - (point.y * math.sin(r))
+            new_y = (point.x * math.sin(r)) + (point.y * math.cos(r))
+            
+            point.x = new_x + x
+            point.y = new_y + y
+
+    def noise_point_grouping(self):
         min_dist = 120
         max_close = 5
         # calculate the quality of points based on their distance from other points
@@ -47,8 +57,17 @@ class Set:
             percent = max_close / amount_within
             point.acc = point.acc * percent
 
+    def noise_point_movement(self, sets):
         # check the similarity with previous point and translate the points according to movement
-                    
+        # an array of sets is passed and the length of that array is what dictates the amount of history checked
+        for set in sets:
+            # translate this point to x=0 y=0
+            set.translate(-set.robot_x, -set.robot_y, 0) # negative because we want to remove the translation
+        
+        # now we need to compare the sets and give all points a value based on the distance or offset from the closest point
+        # if the point doesnt have a close point then it will receive a low acc score even though this may be due to other lidar issues
+        
+        
 
     def string(self):
         string = ""
